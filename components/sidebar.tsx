f153f60344c25097db129a4414c03e0539f0dde1
Haris1245 <haris.v.photography@gmail.com>
2023-10-19 15:38:34 +0300

    changedalt

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -144,7 +144,7 @@ const SideBar = ({ apiLimitCount = 0 ,isPro = false}: SidebarProps) => {
       <div className="px-3 py-2 flex-1">
         <Link href="/dashboard" className="flex items-center pl-3 mb-14">
           <div className="relative mr-4">
-            <img alt="logo" src="big-logo.svg" width={500} height={500} />
+            <img alt="refresh" src="big-logo.svg" width={500} height={500} />
           </div>
         </Link>
         <div className="space-y-1">
