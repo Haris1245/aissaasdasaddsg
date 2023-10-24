6a9fe17ec798a23a55d231b08913b684da45d90a
Haris1245 <haris.v.photography@gmail.com>
2023-10-24 15:48:37 +0300

    sfgf

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -7,7 +7,7 @@ const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   const apiLimitCount = await getApiLimitCount();
   const isPro = await checkSubscription()
   return (
-    <div className="h-full relative ">
+    <div className="h-full relative overflow-scroll">
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y0  bg-[#121212]">
         <div className="text-white">
           <SideBar apiLimitCount={apiLimitCount} isPro={isPro}/>
