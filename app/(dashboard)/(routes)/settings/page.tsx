462c651faa1fc97f0738926fafbd1f7b7e0157ae
Haris1245 <haris.v.photography@gmail.com>
2023-10-25 20:24:05 +0300

    ffsdgs

--- app/(dashboard)/(routes)/settings/page.tsx
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -1,33 +1,13 @@
-"use client"
+
 
 import Heading from "@/components/heading";
-import { Settings, Zap } from "lucide-react";
+import { Settings } from "lucide-react";
 import {checkSubscription} from "@/lib/subscription"
 import SubscriptionButton from "@/components/subscription-button";
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
-import toast from "react-hot-toast";
-import axios from "axios";
 const SettingsPage =async () => {
-  const [loading, setLoading] = useState(false)
-
   const isPro = await checkSubscription();
-  const onSubscribe = async () => {
-    try{
-      setLoading(true)
-      const response = await axios.get("/api/stripe");
   
-      window.location.href =  response.data.url;
-    }
-  catch(error){
-  
-      toast.error("Something went wrong!")
-    
-  }
-  finally{
-    setLoading(false)
-  }
-  }
   return (
     <div>
       <Heading
@@ -40,9 +20,7 @@ const SettingsPage =async () => {
         <div className="text-muted-foreground text-sm">
           {isPro ? "You are currently on a Pro plan." : "You are currently on a free plan"}
         </div>
-        <Button  disabled={loading} size="lg" variant="premium" className="w-20 " onClick={onSubscribe}>
-            Go Pro <Zap className="w-4 h-4 ml-2 fill-white" />
-          </Button>
+       <a href={isPro ? "https://billing.stripe.com/p/login/fZe1771HQafF2cMaEE" : ""}><SubscriptionButton isPro={isPro}/></a>
         <div className="block">
           {isPro ? <a href="https://discord.gg/2tP4wsHF27" className="ml-0.5 font-extrabold"><Button  className="bg-orange-500 hover:bg-orange-800">DISCORD SERVER</Button></a> : <h1 className="text-black font-bold">DISCORD_SERVER - Pro Plan</h1>}
         </div>
