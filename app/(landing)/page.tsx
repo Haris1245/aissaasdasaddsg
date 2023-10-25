fb635f6f15ff54ba1fb7a34ebbbb8ec703d2edeb
Haris1245 <haris.v.photography@gmail.com>
2023-10-25 17:45:21 +0300

    sggs

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -10,7 +10,7 @@ const LandingPage = () => {
       <a href="https://www.producthunt.com/posts/winnerai?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-winnerai" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=421787&theme=light" alt="WinnerAI - WinnerAI&#0032;is&#0032;an&#0032;all&#0032;in&#0032;one&#0032;AI&#0032;model&#0032;platform | Product Hunt" className="w-[250px] h-[54px] block m-auto"  /></a>
       <LandingHero />
       <LandingContent />
-
+      <iframe className="border-0 block m-auto" src="https://cards.producthunt.com/cards/posts/421787?v=1" width="500" height="405"  ></iframe>
       <LandingFooter />
     </div>
   );
