b1af56a62c7d86913b873cf9135744d55e1e0782
Haris1245 <haris.v.photography@gmail.com>
2023-10-30 15:38:00 +0200

    Fix:made the logo on the sidebar go to /

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -131,7 +131,7 @@ const SideBar = ({ apiLimitCount = 0 ,isPro = false}: SidebarProps) => {
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#121212] text-white overflow-scroll">
       <div className="px-3 py-2 flex-1">
-        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+        <Link href="/" className="flex items-center pl-3 mb-14">
           <div className="relative mr-4">
             <img alt="refresh" src="big-logo.svg" width={500} height={500} />
           </div>
